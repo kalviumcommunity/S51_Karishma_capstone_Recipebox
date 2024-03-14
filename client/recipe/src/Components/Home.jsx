f308036bb9baf900d5ef86014bad9f3dd8b8f8a5
karishmaaaaa <karishma.ss@kalvium.community>
2024-03-14 12:17:39 +0530

    adding navbar

---
+++ client/recipe/src/Components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      {/* <h1>Home Page</h1> */}
+      <h1>Welcome to Recipe Box.</h1>
+      <p>This is place where you can find your dish for your </p>
+
+    </div>
+  );
+}
+
+export default Home;
