7b8763e74dd329d5d41ebb3292d0f0d093d0bc89
karishmaaaaa <karishma.ss@kalvium.community>
2024-04-01 10:25:59 +0530

    footer change

--- client/recipe/src/Components/Footer.jsx
+++ client/recipe/src/Components/Footer.jsx
@@ -1,24 +1,14 @@
-  import React from 'react';
-  import { Link } from 'react-router-dom';
-  import './Footer.css'; 
+import React from 'react';
+import './Footer.css'; 
 
-  function Footer() {
-    return (
-      <footer className="footer">
-        <div className="box">
-          <div className="row">
-            <div className="column">
-              <h3 className="feedback-heading">Feedback</h3>
-              <p className="feedback-text">Your thoughts and feedback are crucial to help us improve.
-              We'd be grateful if you could take a moment to share your insights with us.</p>
-              <Link to="/feedback" className="feedback-btn">Give Feedback</Link>
-            </div>
-          </div>
-          <hr className="hr" />
-          <p className="copyright">&copy; 2024 Recipe Box. All rights reserved.</p>
-        </div>
-      </footer>
-    );
-  }
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="box">
+        <p>&copy; {new Date().getFullYear()} Recipe Box</p>
+      </div>
+    </footer>
+  );
+}
 
-  export default Footer;
+export default Footer;
