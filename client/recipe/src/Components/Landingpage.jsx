fb92794b392e3f21af0a9612981b9d94facdd858
karishmaaaaa <karishma.ss@kalvium.community>
2024-04-04 11:49:47 +0530

    example recipe feature

--- client/recipe/src/Components/Landingpage.jsx
+++ client/recipe/src/Components/Landingpage.jsx
@@ -3,6 +3,13 @@ import logo from '../assets/logo.png';
 import search from '../assets/search.png';
 import user from '../assets/user.png';
 import front_meal from '../assets/front_meal.png';
+import creamy_salad from '../assets/creamy_salad.png';
+import toffu from '../assets/toffu.png';
+import potatoes from '../assets/potatoes.png';
+import mushroom_soup from '../assets/mushroom_soup.png';
+import pancake from '../assets/pancake.png';
+import teriyaki from '../assets/teriyaki.png';
+
 
 function Landingpage() {
     return (
@@ -36,13 +43,57 @@ function Landingpage() {
                         <img src={front_meal} alt="" />
                     </div>
                 </div>
-                {/* <div className="recipe_side">
+                <div className="recipe_side">
                     <div className="heading">
                         <h1 className="ways">Discover, Create, Share</h1>
-                        <p className="popular">Check our most popular recipes of this week</p>
+                        
                         <button className="see_all">See All</button>
                     </div>
-                </div> */}
+                    <div>
+                    <p className="popular">Check our most popular recipes of this week</p>
+                    </div>
+                    <div className="ex_recipe">
+                        
+                            <div className="r_box">
+                            <img src={creamy_salad} className="r_img" alt="" />
+                            <div className="r_head">
+                                <p className="r_name">Creamy Salad</p>
+                                </div>
+                            </div>
+                            <div className="r_box">
+                                <img src={toffu} className="r_img" alt="" />
+                                <div className="r_head">
+                                    <p className="r_name">Tofu Tomatoes Soup </p>
+                                </div>
+                            </div>
+                            <div className="r_box">
+                                <img src={potatoes}  className="r_img" alt="" />
+                                <div className="r_head">
+                                    <p className="r_name">Crunchy Potatoes</p>
+                                </div>
+                            </div>
+                            <div className="r_box">
+                                <img src={mushroom_soup} className="r_img" alt="" />
+                                <div className="r_head">
+                                    <p className="r_name">Mushroom Soup</p>
+                                </div>
+                            </div>
+                            <div className="r_box"> 
+                                <img src={pancake} className="r_img" alt="" />
+                                <div className="r_head">
+                                    <p className="r_name">Raspberry Pancake</p>
+                                </div>
+                            </div>
+                            <div className="r_box">
+                                <img src={teriyaki} className="r_img" alt="" />
+                                <div className="r_head">
+                                    <p className="r_name">Beef Teriyaki</p>
+                                </div>
+                            </div>
+                        
+                    </div>
+
+                </div>
             </div>
         </>
     )
