5bc283ea962338a374167ca352bcf64fddcbd48a
karishmaaaaa <karishma.ss@kalvium.community>
2024-04-12 16:01:41 +0530

    chaning the about us page

--- client/recipe/src/Components/About.jsx
+++ client/recipe/src/Components/About.jsx
@@ -3,14 +3,16 @@ import './About.css'; // Make sure the path is correct
 
 function About() {
   return (
-    <div className="container">
-      <h1>Welcome to Recipe Box.</h1>
-      <p>We are excited to be your cooking partners on your cooking journey. Cooking is more about an expression of self through food.</p>
-      <p>Here you can explore, share and enjoy flavorful meals which that serves as a flexible platform for both experienced cooks and people who are starting their cooking journey.</p>
-      <h2>What makes us different ?</h2>
-      <p>You can easily manage your recipes using our user-friendly platform. You will have the ability to update or delete any recipe, just by clicking a couple of buttons.</p>
-      <h1>Therefore, let’s cook and create together!</h1>
+    <div id='about'>
+    <div className="a_container">
+      <h1 className='a_head'>Welcome to Recipe Box.</h1>
+      <p className='a_para'>We are excited to be your cooking partners on your cooking journey. Cooking is more about an expression of self through food.</p>
+      <p className='a_para'>Here you can explore, share and enjoy flavorful meals which that serves as a flexible platform for both experienced cooks and <br />people who are starting their cooking journey.</p>
+      <h1 className='a_head'>What makes us different ?</h1>
+      <p className='a_para'>You can easily manage your recipes using our user-friendly platform. You will have the ability to add or remove <br />any recipe to your liked list, just by clicking a couple of buttons. <br />you can also view the cooking instruction video with the given youtube link</p>
+      <h1 className='a_head'>Therefore, let’s cook and create together!</h1>
       
+    </div>
     </div>
   );
 }
