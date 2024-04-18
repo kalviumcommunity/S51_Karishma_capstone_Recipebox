a896e5d5f140a9b77316a5632cce2d185b2bc70d
karishmaaaaa <karishma.ss@kalvium.community>
2024-04-18 11:30:50 +0530

    adding navbar to recipe page

--- client/recipe/src/Components/Recipes.jsx
+++ client/recipe/src/Components/Recipes.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
-
-
+import Navbar from './Navbar'
+import './Recipes.css'
 function Recipes() {
   return (
+  <>
+    <Navbar />
     <div>Recipe</div>
+  </>
+    
     )
 }
 export default Recipes
