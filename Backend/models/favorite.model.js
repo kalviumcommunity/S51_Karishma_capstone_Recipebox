2947928dbedf13e8ff51cf09a8c2083c95adc013
karishmaaaaa <karishma.ss@kalvium.community>
2024-05-10 11:22:40 +0530

    authentication for user

--- Backend/models/favorite.model.js
+++ Backend/models/favorite.model.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const FavoriteSchema = new mongoose.Schema({
+    username:{type: String,required: true},
     idMeal: {type: Number,required: true},
     strMeal: {type: String,required: true},
     strMealThumb:{type: String,required: true} 
