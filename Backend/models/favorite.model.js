3fb47b3cc34291f02c7ec5c1292fe23de430153a
karishmaaaaa <karishma.ss@kalvium.community>
2024-05-13 13:48:45 +0530

    creating backend for create recipe

--- Backend/models/favorite.model.js
+++ Backend/models/favorite.model.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 
 const FavoriteSchema = new mongoose.Schema({
     username:{type: String,required: true},
-    idMeal: {type: Number,required: true},
-    strMeal: {type: String,required: true},
-    strMealThumb:{type: String,required: true} 
+    idMeal: {type: Number,required: true,unique:true},
+    strMeal: {type: String,required: true,unique:true},
+    strMealThumb:{type: String,required: true,unique:true} 
 },
 {
     timestamps: true
