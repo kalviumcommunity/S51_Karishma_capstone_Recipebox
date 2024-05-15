cae8e9054ec68e3724f6c2b171c35cc637fdc7f9
karishmaaaaa <karishma.ss@kalvium.community>
2024-05-15 15:33:02 +0530

    form created for creating recipe

--- client/recipe/src/Components/Createrecipe.jsx
+++ client/recipe/src/Components/Createrecipe.jsx
@@ -1,6 +1,10 @@
 import axios from 'axios'
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import Navbar from './Navbar'
+import './Createrecipe.css'
+
+
 
 export default function Createrecipe() {
   const [name,setName]=useState()
@@ -22,30 +26,35 @@ export default function Createrecipe() {
   
   }
   return (
+    <div className='f_recipe'>
+        <Navbar/>
     
-    <div>
-      <form onSubmit={Submit}>
-        <div>
+
+    <div className='fd_recipe'>
+      <h1 className='hf_recipe'>ADD YOUR RECIPE</h1>
+      <form className=" fill_f" onSubmit={Submit}>
+        <div className='n_recipe'>
           <span>
           Recipe Name:
         </span><input type="text" onClick={(e)=>{setName(e.target.value)}} />
         </div>
-        {/* <div>
+        {/* <div className="i_recipe">
           <span>
           Recipe image:
         </span><input type="text"  onClick={(e)=>{setName(e.target.value)}}/>
         </div> */}
-        <div>
+        <div className='y_recipe'>
           <span>
           Youtube Link:
         </span><input type="text"  onClick={(e)=>{setYoutube(e.target.value)}}/>
         </div>
-        <div>
+        <div className='i_recipe' >
           <span>
           Ingredient:
         </span><input type="text"  onClick={(e)=>{setIngredient(e.target.value)}} />
         </div>
       </form>
+    </div>
     </div>
   )
 }
